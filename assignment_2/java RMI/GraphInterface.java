71fbe2cfc5d873b030fe6b8d612d09767fba6289
rajatverma09 <rajat.verma@students.iiit.ac.in>
2020-02-15 16:37:23 +0530

    java RMI removed

--- assignment_2/java RMI/GraphInterface.java
+++
@@ -1,9 +0,0 @@
-import java.rmi.Remote; 
-import java.rmi.RemoteException;  
-
-// Creating Remote interface for our application 
-public interface GraphInterface extends Remote {  
-   String addGraph(String id, int n) throws RemoteException;  
-   String addEdge(String id, int u, int v, int w) throws RemoteException;  
-   int getMST(String id) throws RemoteException;
-} 
